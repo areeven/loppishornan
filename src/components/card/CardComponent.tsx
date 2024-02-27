739c7808930573a8b2f01640fbe7289977696d59
areeven <emilie.ost1990@gmail.com>
2024-02-27 19:39:36 +0100

    Added shop

--- src/components/card/CardComponent.tsx
+++ src/components/card/CardComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import "./CardComponent.scss";
 import { cardData } from "../../shared/data/cardData";
@@ -8,18 +8,34 @@ interface CardProps {
 }
 
 const CardComponent: React.FC<CardProps> = ({ group }) => {
+  const [open, setOpen] = useState<boolean>(false);
+  function handleOpen() {
+    setOpen(!open);
+  }
+
   return (
     <motion.div>
-      <h1>{group}</h1>
-      {cardData
-        .filter((data) => data.group === group)
-        .map((data) => (
-          <div key={data.label}>
-            <label>
+      <div className="card-container">
+        {cardData
+          .filter((data) => data.group === group)
+          .map((data) => (
+            <div key={data.label}>
               <p>{data.label}</p>
-            </label>
-          </div>
-        ))}
+              <img
+                onClick={handleOpen}
+                className="shop-image"
+                src={data.image}
+                alt={data.label}
+                width="200px"
+              />
+              <p>{data.price}</p>
+            </div>
+          ))}
+        <div
+          className={!open ? "open-window" : "closed-window"}
+          onClick={handleOpen}
+        ></div>
+      </div>
     </motion.div>
   );
 };
--- src/shared/data/cardData.ts
+++ src/shared/data/cardData.ts
@@ -1,5 +1,33 @@
+import Chair from "../images/chair.png";
+import Table from "../images/table.png";
+import Books from "../images/books.png";
+
 export const cardData = [
-  { id: 1, group: "card", label: "Label", defaultValue: "" },
-  { id: 2, group: "card", label: "Image", defaultValue: "" },
-  { id: 3, group: "card", label: "Description", defaultValue: "" },
+  {
+    id: 1,
+    group: "card",
+    label: "Stol",
+    defaultValue: "",
+    image: Chair,
+    price: "300sek",
+    description: "",
+  },
+  {
+    id: 2,
+    group: "card",
+    label: "Bord",
+    defaultValue: "",
+    image: Table,
+    price: "800sek",
+    description: "",
+  },
+  {
+    id: 3,
+    group: "card",
+    label: "Böcker",
+    defaultValue: "",
+    image: Books,
+    price: "50sek / each",
+    description: "",
+  },
 ];
